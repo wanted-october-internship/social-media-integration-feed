61dc759a19253376569a043061da88ef197fc0ce
cousim46 <wjdghldns24@naver.com>
2023-10-25 22:48:48 +0900

    Add : 회원가입 상태 필드 추가

--- src/main/java/intergration/feed/account/domain/Account.java
+++ src/main/java/intergration/feed/account/domain/Account.java
@@ -1,8 +1,11 @@
-package intergration.feed.account;
+package intergration.feed.account.domain;
 
+import intergration.feed.account.domain.type.JoinStatus;
 import intergration.feed.common.BaseEntity;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -18,4 +21,13 @@ public class Account extends BaseEntity {
     private String email;
     @Column(nullable = false)
     private String password;
+    @Enumerated(value = EnumType.STRING)
+    private JoinStatus joinStatus;
+
+    public Account(String loginId, String email, String password) {
+        this.loginId = loginId;
+        this.email = email;
+        this.password = password;
+        joinStatus = JoinStatus.READY;
+    }
 }

ce776f31ef77e504e0e8c9834cd2af1e1b8b07db
cousim46 <wjdghldns24@naver.com>
2023-10-25 23:44:17 +0900

    Add : 로그인 아이디와 회원가입상태에 대한 메서드 쿼리 추가

--- src/main/java/intergration/feed/account/AccountRepository.java
+++ src/main/java/intergration/feed/account/AccountRepository.java
@@ -1,9 +1,14 @@
 package intergration.feed.account;
 
 import intergration.feed.account.domain.Account;
+import intergration.feed.account.domain.type.JoinStatus;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface AccountRepository extends JpaRepository<Account,Long> {
 
     boolean existsByLoginId(String loginId);
+
+    Optional<Account> findByLoginIdAndJoinStatus(String loginId, JoinStatus joinStatus);
 }

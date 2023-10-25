a787150f0ac0d4cf24a1334164b926728463db73
cousim46 <wjdghldns24@naver.com>
2023-10-25 22:49:32 +0900

    Add : 암호화 클래스 빈으로 등록

--- src/main/java/intergration/feed/FeedApplication.java
+++ src/main/java/intergration/feed/FeedApplication.java
@@ -2,11 +2,18 @@ package intergration.feed;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 @EnableJpaAuditing
 public class FeedApplication {
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(FeedApplication.class, args);

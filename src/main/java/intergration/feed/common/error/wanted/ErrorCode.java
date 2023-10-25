279f0e4b755799b7c884cf324e9ee9af35263dee
cousim46 <wjdghldns24@naver.com>
2023-10-25 23:44:53 +0900

    Add : 에러코드 추가

--- src/main/java/intergration/feed/common/error/wanted/ErrorCode.java
+++ src/main/java/intergration/feed/common/error/wanted/ErrorCode.java
@@ -8,6 +8,13 @@ import org.springframework.http.HttpStatus;
 @Getter
 @RequiredArgsConstructor
 public enum ErrorCode {
+    NOT_FOUND_ACCOUNT(HttpStatus.NOT_FOUND,"회원이 존재하지 않습니다."),
+    NOT_FOUND_CERT_CODE(HttpStatus.NOT_FOUND,"인증코드가 존재하지 않습니다."),
+
+    NOT_EQUALS_CERT_CODE(HttpStatus.BAD_REQUEST, "인증코드가 일치하지 않습니다."),
+    MUST_NOT_BE_ALL_DIGITS(HttpStatus.BAD_REQUEST, "비밀번호는 숫자로만 이루어질 수 없습니다."),
+    NO_REPEAT_CHARACTERS_AND_MULTIPLE_CRITERIA(HttpStatus.BAD_REQUEST,"비밀번호는 숫자, 문자, 특수 문자 중 최소 2가지를 포함하며, 3회 이상 연속된 문자는 사용할 수 없습니다."),
+
     ALREADY_LOGIN_ID(HttpStatus.CONFLICT, "이미 존재하는 아이디입니다.");
 
     private final HttpStatus status;

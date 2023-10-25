75d626dabf6e95ad399a3434c3f4c92501a438e5
cousim46 <wjdghldns24@naver.com>
2023-10-25 23:43:56 +0900

    Add : 인증코드 확인 API 추가ˆ

--- src/main/java/intergration/feed/account/AccountController.java
+++ src/main/java/intergration/feed/account/AccountController.java
@@ -1,9 +1,11 @@
 package intergration.feed.account;
 
 import intergration.feed.account.dto.AccountRequestDto.Join;
+import intergration.feed.account.dto.AccountRequestDto.ValidateCertCode;
 import javax.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,10 +17,16 @@ import org.springframework.web.bind.annotation.RestController;
 public class AccountController {
 
     private final AccountWriteService accountWriteService;
+
     @PostMapping
     public ResponseEntity<String> join(@Valid @RequestBody Join join){
         accountWriteService.join(join);
         return ResponseEntity.status(201).body("회원가입 완료");
+    }
+
+    @GetMapping
+    public void validateCertCode(ValidateCertCode validateCertCodeRequest) {
+        accountWriteService.validateCertCode(validateCertCodeRequest);
     }
 
 }

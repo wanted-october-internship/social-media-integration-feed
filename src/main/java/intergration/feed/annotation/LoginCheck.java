5d96c17a858804c4d55e2995af67bb15ae4acad8
cousim46 <wjdghldns24@naver.com>
2023-10-29 21:20:28 +0900

    Add : 로그인 정보를 받기위한 커스텀 어노테이션 생성

---
+++ src/main/java/intergration/feed/annotation/LoginCheck.java
@@ -0,0 +1,12 @@
+package intergration.feed.annotation;
+
+import static java.lang.annotation.ElementType.PARAMETER;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(PARAMETER)
+public @interface LoginCheck {
+}

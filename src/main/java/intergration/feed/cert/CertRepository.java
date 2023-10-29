2eefb675eaf9237115898843f3df214338c66758
cousim46 <wjdghldns24@naver.com>
2023-10-29 16:32:21 +0900

    Add : 유저에 대한 인증코드 조회하는 메서드 쿼리 추가

--- src/main/java/intergration/feed/cert/CertRepository.java
+++ src/main/java/intergration/feed/cert/CertRepository.java
@@ -1,7 +1,11 @@
 package intergration.feed.cert;
 
+import intergration.feed.account.domain.Account;
 import intergration.feed.cert.domain.Cert;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CertRepository extends JpaRepository<Cert, Long> {
+
+    Optional<Cert> findByAccount(Account account);
 }

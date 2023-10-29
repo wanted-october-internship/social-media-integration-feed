2298fc8b97923273dd447aa4a017d8beee333ac1
cousim46 <wjdghldns24@naver.com>
2023-10-29 21:23:44 +0900

    Add : jwt 토큰에 대한 필터기능 생성

---
+++ src/main/java/intergration/feed/config/jwt/JwtTokenFilter.java
@@ -0,0 +1,54 @@
+package intergration.feed.config.jwt;
+
+import static intergration.feed.common.error.wanted.ErrorCode.NOT_FOUND_ACCOUNT;
+
+import intergration.feed.account.AccountRepository;
+import intergration.feed.account.domain.Account;
+import intergration.feed.common.error.wanted.WantedException;
+import java.io.IOException;
+import java.util.List;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpHeaders;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+@RequiredArgsConstructor
+public class JwtTokenFilter extends OncePerRequestFilter {
+    private final AccountRepository accountRepository;
+    @Value("${spring.jwt.secret-key}")
+    private final String secretKey;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+        throws ServletException, IOException {
+        String authorizationHeader = request.getHeader(HttpHeaders.AUTHORIZATION);
+
+        if(authorizationHeader == null || !authorizationHeader.startsWith("Bearer ")) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+        String token = authorizationHeader.split(" ")[1];
+
+        if(JwtTokenUtil.isExpired(token, secretKey)) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        Account loginUser = accountRepository.findByLoginId(JwtTokenUtil.getLoginId(token, secretKey)).orElseThrow(() -> new WantedException(NOT_FOUND_ACCOUNT));
+        UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(
+            loginUser.getLoginId(), null, List.of(new SimpleGrantedAuthority(loginUser.getRole().name())));
+        authenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+
+        // 권한 부여
+        SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+        filterChain.doFilter(request, response);
+    }
+}

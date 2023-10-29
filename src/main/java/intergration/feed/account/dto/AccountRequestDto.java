89038ed1fd2db68aa7fe751ce9436fec3e60425b
cousim46 <wjdghldns24@naver.com>
2023-10-29 16:32:54 +0900

    Add : 회원가입, 인증코드 검증에 대한 dto 추가ˆ

---
+++ src/main/java/intergration/feed/account/dto/AccountRequestDto.java
@@ -0,0 +1,51 @@
+package intergration.feed.account.dto;
+
+import intergration.feed.account.domain.Account;
+import intergration.feed.account.domain.type.Role;
+import intergration.feed.cert.domain.Cert;
+import intergration.feed.common.error.wanted.ErrorCode;
+import intergration.feed.common.error.wanted.WantedException;
+import intergration.feed.validate.Password;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+public class AccountRequestDto {
+    @Getter
+    @AllArgsConstructor
+    public static class Join {
+
+        @NotBlank
+        private String loginId;
+
+        @NotBlank
+        private String password;
+
+        @Email
+        @NotBlank
+        private String email;
+
+        public void encryptPassword(PasswordEncoder bCryptPasswordEncoder) {
+            Password.validate(password);
+            password = bCryptPasswordEncoder.encode(password);
+        }
+
+        public Account toEntity(Role role) {
+            return Account.create(loginId,email,password,role);
+        }
+    }
+    @Getter
+    @AllArgsConstructor
+    public static class ValidateCertCode {
+        private String loginId;
+        private String certCode;
+
+        public void equalsCode(Cert cert) {
+            if(cert.getCode().equals(certCode)) {
+                throw new WantedException(ErrorCode.NOT_EQUALS_CERT_CODE);
+            }
+        }
+    }
+}

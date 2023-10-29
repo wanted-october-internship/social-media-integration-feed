a3b9cf9ae82050857141280b29251f1e96798058
cousim46 <wjdghldns24@naver.com>
2023-10-29 21:25:15 +0900

    Add : 로그인 기능 구현

---
+++ src/main/java/intergration/feed/account/AccountReadService.java
@@ -0,0 +1,35 @@
+package intergration.feed.account;
+
+import static intergration.feed.common.error.wanted.ErrorCode.NOT_EQUALS_LOGIN_INFO;
+
+import intergration.feed.account.domain.Account;
+import intergration.feed.account.dto.AccountRequestDto.Login;
+import intergration.feed.common.error.wanted.WantedException;
+import intergration.feed.config.jwt.JwtTokenUtil;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class AccountReadService {
+
+    private final AccountRepository accountRepository;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+    @Value("${spring.jwt.secret-key}")
+    private  String jwtKey;
+
+    public String executeLogin(Login login) {
+        Account loginRequestAccont = accountRepository.findByLoginId(login.getLoginId())
+            .orElseThrow(() -> new WantedException(NOT_EQUALS_LOGIN_INFO));
+        if(!bCryptPasswordEncoder.matches(login.getPassword(),loginRequestAccont.getPassword())) {
+            throw new WantedException(NOT_EQUALS_LOGIN_INFO);
+        }
+        return  JwtTokenUtil.create(login.getLoginId(), jwtKey, 30000);
+
+    }
+
+}

1f8700358b50b3cf402049aea0e820475d221b71
cousim46 <wjdghldns24@naver.com>
2023-10-29 21:18:29 +0900

    Add : 시큐리티 설정

--- src/main/java/intergration/feed/config/SecurityConfig.java
+++ src/main/java/intergration/feed/config/SecurityConfig.java
@@ -1,16 +1,29 @@
 package intergration.feed.config;
 
+import intergration.feed.account.AccountRepository;
+import intergration.feed.config.jwt.JwtTokenFilter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final AccountRepository accountRepository;
+    @Value("${spring.jwt.secret-key}")
+    private String jwtKey;
 
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable()
+            .httpBasic().disable()
+            .addFilterBefore(new JwtTokenFilter(accountRepository, jwtKey),
+                UsernamePasswordAuthenticationFilter.class)
             .formLogin()
             .loginProcessingUrl("/login");
     }

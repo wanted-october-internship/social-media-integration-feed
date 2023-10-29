13bdd0a79c03d7d9adae4f00573173621d46df2c
cousim46 <wjdghldns24@naver.com>
2023-10-29 21:20:45 +0900

    Add : jwt 토큰 발급 및 유효시간 설정

---
+++ src/main/java/intergration/feed/config/jwt/JwtTokenUtil.java
@@ -0,0 +1,39 @@
+package intergration.feed.config.jwt;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import java.util.Date;
+
+public class JwtTokenUtil {
+
+    //jwt 토큰 생성
+    public static String create(String loginId, String key, long expireAt) {
+        Claims claims = Jwts.claims();
+        claims.put("loginId", loginId);
+
+        return Jwts.builder()
+            .setClaims(claims)
+            .setIssuedAt(new Date(System.currentTimeMillis()))
+            .setExpiration(new Date(System.currentTimeMillis() + expireAt))
+            .signWith(SignatureAlgorithm.HS256,key)
+            .compact();
+    }
+
+    // SecretKey를 사용해 Token Parsing
+    private static Claims extractClaims(String token, String secretKey) {
+        return Jwts.parser().setSigningKey(secretKey).parseClaimsJws(token).getBody();
+    }
+
+    // Claims에서 loginId 꺼내기
+    public static String getLoginId(String token, String secretKey) {
+        return extractClaims(token, secretKey).get("loginId").toString();
+    }
+
+    // 밝급된 Token이 만료 시간이 지났는지 체크
+    public static boolean isExpired(String token, String secretKey) {
+        Date expiredDate = extractClaims(token, secretKey).getExpiration();
+        // Token의 만료 날짜가 지금보다 이전인지 check
+        return expiredDate.before(new Date());
+    }
+}

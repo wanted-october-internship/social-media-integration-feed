e412e72249b7b82a28643a5127359913d1dfa974
cousim46 <wjdghldns24@naver.com>
2023-10-29 21:24:41 +0900

    Add : 로그인 아이디로 유저 찾는 메서드 쿼리 추가

--- src/main/java/intergration/feed/account/AccountRepository.java
+++ src/main/java/intergration/feed/account/AccountRepository.java
@@ -4,11 +4,11 @@ import intergration.feed.account.domain.Account;
 import intergration.feed.account.domain.type.JoinStatus;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 public interface AccountRepository extends JpaRepository<Account,Long> {
 
     boolean existsByLoginId(String loginId);
 
     Optional<Account> findByLoginIdAndJoinStatus(String loginId, JoinStatus joinStatus);
+    Optional<Account> findByLoginId(String loginId);
 }

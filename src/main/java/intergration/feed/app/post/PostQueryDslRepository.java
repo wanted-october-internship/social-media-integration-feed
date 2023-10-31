5872ac4e9f1f1c5f4f6a6fa702fdd27c0e12a731
cousim46 <wjdghldns24@naver.com>
2023-10-31 20:37:46 +0900

    Add : QueryDDSLRepository 인터페이스 생성

---
+++ src/main/java/intergration/feed/app/post/PostQueryDslRepository.java
@@ -0,0 +1,9 @@
+package intergration.feed.app.post;
+
+import intergration.feed.app.post.domain.Post;
+import intergration.feed.app.post.dto.PostRequestDto.Filter;
+import java.util.List;
+
+public interface PostQueryDslRepository {
+    List<Post> findAllPost(Filter filter);
+}

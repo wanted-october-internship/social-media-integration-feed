4f7117171097b5d9f09aa82d0560205349711879
cousim46 <wjdghldns24@naver.com>
2023-10-31 21:19:35 +0900

    Fix : 메서드 쿼리 추가

--- src/main/java/intergration/feed/app/history/ViewHistoryRepository.java
+++ src/main/java/intergration/feed/app/history/ViewHistoryRepository.java
@@ -1,7 +1,15 @@
 package intergration.feed.app.history;
 
+import intergration.feed.app.account.domain.Account;
 import intergration.feed.app.history.domain.ViewHistory;
+import intergration.feed.app.post.domain.Post;
+import java.util.Collection;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ViewHistoryRepository extends JpaRepository<ViewHistory, Long> {
+
+    int countByAccountAndPost(Account account, Post post);
+
+    List<ViewHistory> findAllByAccountAndPostIsNotNullOrderByCreatedAtDesc(Account account);
 }

407561f15e6e65b7300f4042ee4a538be72d1ea7
cousim46 <wjdghldns24@naver.com>
2023-10-31 20:40:51 +0900

    Add : 트위터에 대한 좋아요, 공유하기 비즈니스 로직 구현

---
+++ src/main/java/intergration/feed/app/sns/TwitterService.java
@@ -0,0 +1,56 @@
+package intergration.feed.app.sns;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.client.RestTemplate;
+
+@Component(value = "twitter")
+@RequiredArgsConstructor
+public class TwitterService implements SnsService {
+
+    @Value("${sns.twitter.like-url}")
+    private String likeUrl;
+
+    @Value("${sns.twitter.share-url}")
+    private String shareUrl;
+
+    @Value("${spring.local}")
+    private boolean isLocal;
+
+    private RestTemplate restTemplate;
+
+    @Override
+    public boolean like(Long postId) {
+        try {
+            MultiValueMap<String, String> parameters = new LinkedMultiValueMap<>();
+            parameters.add("jeong", "test");
+            restTemplate.postForEntity(likeUrl + postId, parameters, String.class);
+            return true;
+        }catch (Exception e) {
+            if(isLocal) {
+                return true;
+            }
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    @Override
+    public boolean share(Long postId) {
+        try {
+            MultiValueMap<String, String> parameters = new LinkedMultiValueMap<>();
+            parameters.add("jeong", "test");
+            restTemplate.postForEntity(shareUrl + postId, parameters, String.class);
+            return true;
+        }catch (Exception e) {
+            if(isLocal) {
+                return true;
+            }
+            e.printStackTrace();
+            return false;
+        }
+    }
+}

24a5c37aff71a85c4c5985c477b75d9b16e4d885
cousim46 <wjdghldns24@naver.com>
2023-10-31 22:13:59 +0900

    Add : 매개변수 추가

--- src/main/java/intergration/feed/app/post/PostQueryDslRepository.java
+++ src/main/java/intergration/feed/app/post/PostQueryDslRepository.java
@@ -5,5 +5,5 @@ import intergration.feed.app.post.dto.PostRequestDto.Filter;
 import java.util.List;
 
 public interface PostQueryDslRepository {
-    List<Post> findAllPost(Filter filter);
+    List<Post> findAllPost(Filter filter, String loginId);
 }

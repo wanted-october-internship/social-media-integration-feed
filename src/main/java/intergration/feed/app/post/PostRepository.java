c5022a2181bed6cfa1f44897fdab55c1ea1abe37
cousim46 <wjdghldns24@naver.com>
2023-10-31 20:38:39 +0900

    Add : PostQueryDslRepository 상속

---
+++ src/main/java/intergration/feed/app/post/PostRepository.java
@@ -0,0 +1,7 @@
+package intergration.feed.app.post;
+
+import intergration.feed.app.post.domain.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostRepository extends JpaRepository<Post, Long>, PostQueryDslRepository {
+}

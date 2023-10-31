7aa0e01530d737a30e54648c507155cb9e9036ca
cousim46 <wjdghldns24@naver.com>
2023-10-31 21:19:32 +0900

    Fix : 메서드 쿼리 추가

--- src/main/java/intergration/feed/app/history/ShareHistoryRepository.java
+++ src/main/java/intergration/feed/app/history/ShareHistoryRepository.java
@@ -1,7 +1,14 @@
 package intergration.feed.app.history;
 
+import intergration.feed.app.account.domain.Account;
 import intergration.feed.app.history.domain.ShareHistory;
+import intergration.feed.app.post.domain.Post;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ShareHistoryRepository extends JpaRepository<ShareHistory, Long> {
+    int countByAccountAndPost(Account account, Post post);
+    void deleteByAccountAndPost(Account account, Post post);
+
+    List<ShareHistory> findAllByAccountAndPostIsNotNullOrderByCreatedAtDesc(Account account);
 }

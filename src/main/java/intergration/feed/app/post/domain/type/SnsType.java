284a96c8594077b6b153207f2874b64bb194c4c4
cousim46 <wjdghldns24@naver.com>
2023-10-31 20:39:38 +0900

    Add : 필드 추가

--- src/main/java/intergration/feed/app/post/domain/type/SnsType.java
+++ src/main/java/intergration/feed/app/post/domain/type/SnsType.java
@@ -4,9 +4,14 @@ import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
 public enum SnsType {
-    FACEBOOK("페이스북", "https://www.facebook.com"), TWITTER("트위터",
-        "https://www.twitter.com"), INSTAGRAM("인스타그램", "https://www.instagram.com"), THREADS("스레드",
-        "https://www.threads.net");
+    FACEBOOK("페이스북", "https://www.facebook.com","facebook"), TWITTER("트위터",
+        "https://www.twitter.com","twitter"), INSTAGRAM("인스타그램", "https://www.instagram.com","instagram"), THREADS("스레드",
+        "https://www.threads.net","threads");
     private final String name;
     private final String url;
+    private final String sns;
+
+    public String getSns() {
+        return sns;
+    }
 }

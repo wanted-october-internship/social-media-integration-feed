27715fe0c4a1bdd6bf2f628f412f7fb650183377
cousim46 <wjdghldns24@naver.com>
2023-10-31 22:14:31 +0900

    Delete : 해시코드에 대한 필터 구현

--- src/main/java/intergration/feed/app/post/PostReadService.java
+++ src/main/java/intergration/feed/app/post/PostReadService.java
@@ -6,6 +6,7 @@ import intergration.feed.app.post.domain.Post;
 import intergration.feed.app.post.dto.PostRequestDto.Filter;
 import intergration.feed.common.error.wanted.WantedException;
 import java.util.List;
+import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
@@ -14,8 +15,8 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
-@Transactional(readOnly = true)
 @RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class PostReadService {
 
     private final PostRepository postRepository;
@@ -24,10 +25,16 @@ public class PostReadService {
         return postRepository.findById(id).orElseThrow(() -> new WantedException(NOT_FOUND_POST));
     }
 
-
-    public Page<Post> getList(Filter filter) {
-        List<Post> posts = postRepository.findAllPost(filter);
+    public Page<Post> getList(String loginId, Filter filter) {
+        List<Post> posts = postRepository.findAllPost(filter, loginId).stream().filter(post -> {
+            if(filter.getHashTag() != null) {
+                return post.getHashTagList().contains(filter.getHashTag());
+            }else {
+                return post.getHashTagList().contains(loginId);
+            }
+        }
+        ).collect(Collectors.toList());
         PageRequest pageRequest = PageRequest.of(filter.getPage(), filter.getPageCount());
-        return new PageImpl<>(posts,pageRequest, posts.size());
+        return new PageImpl<>(posts, pageRequest, posts.size());
     }
 }
